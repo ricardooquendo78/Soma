b41fcffd78463de07fc7ef24ef45d9dd2faff7f6
RicardoOquendo1 <133079872+ricardooquendo78@users.noreply.github.com>
2026-06-29 11:49:45 -0500

    importar vite dinamicamente para evitar caida de funcion serverless en vercel

--- server.ts
+++ server.ts
@@ -1,7 +1,6 @@
 import express from 'express';
 import path from 'path';
 import dotenv from 'dotenv';
-import { createServer as createViteServer } from 'vite';
 import {
   initDatabase,
   getUsers,
@@ -304,7 +303,8 @@ async function startServer() {
 
   // Vite Integration
   if (process.env.NODE_ENV !== 'production') {
-    const vite = await createViteServer({
+    const { createServer } = await import('vite');
+    const vite = await createServer({
       server: { middlewareMode: true },
       appType: 'spa',
     });
